ff1d85bea6155eb800df9be1291f777e13302d5a
CleqrZ <dylangrangeon34@gmail.com>
2021-04-02 16:54:33 +0200

    presentation

---
+++ web/pages/presentation.php
@@ -0,0 +1,32 @@
+
+    <head>
+        <title>Présentation de notre site</title>
+    </head>
+    <body>
+        <div class="body">
+            
+            <div class="prese_horaire">
+                <div class="presentation">
+                    <h3>Présentation</h3>
+                    <p class="presentation_site">
+                        Bienvenue sur ce site, vous pourrez acheter tous types de bières, mais également des futs de bières, des apéros et autres.  
+                    </p>
+                </div>
+                
+                <div class="horaire">
+                    <p class="horaire_site">
+                        
+                    </p>
+                </div>
+            </div> 
+            <div class="localisation">
+                <img src="" width="" height="" alt="alt"/>
+            </div>
+        </div>     
+    </body>
+</html>
+
+
+
+
+
