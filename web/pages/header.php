9db82c5c0d92bf56a6e2aa76844f3757756efbd6
Nathim <nathimrichar@gmail.com>
2021-04-02 16:54:07 +0200

    header

---
+++ web/pages/header.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <div class="titre">
+                <h1> Prestachope </h1>
+            </div>
+            <div class="btn">
+                <a href="index.php?pages=connexion"  value="connexion" >Connexion</a>
+                <a href="index.php?pages=inscription"  value="inscription" >Inscription</a>
+            </div>
+            <div class="dropdown">
+                <ul>
+                    <li>
+                        Bieres
+                        <ul>
+                            <li>Blonde</li>
+                            <li>Blanche</li>
+                            <li>ambré</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Futs
+                        <ul>
+                            <li>Futs artisanales</li>
+                            <li>Futs industriels</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Apéro
+                        <ul>
+                            <li>Gateaux apéritifs</li>
+                            <li>Olives</li>
+                            <li>Charcuteries</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Autre
+                        <ul>
+                            <li> t-shirt</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    </body>
+</html>
+
+
+
