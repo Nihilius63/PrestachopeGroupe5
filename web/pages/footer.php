8ae99b2468e669d9c71956a35edb39540bfe921d
CleqrZ <dylangrangeon34@gmail.com>
2021-04-09 17:11:58 +0200

    footer fini + présentation (je referais la pres)

---
+++ web/pages/footer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+	<div class="footer" style="background-color: black; text-align: center;">
+		<div class="création" style="color: white;">
+			<div class="cree">
+				<H2 class=""> Création Par :</H2>
+			</div>
+			<hr>
+			<div class="personne" style="display: flex;justify-content: space-around;">
+				<div class="perso1">
+					<p>Nathim</p>
+				</div>
+				<hr style=" margin: unset;">
+				<div class="perso2">
+					<p>Lucas</p>
+				</div>
+				<hr style=" margin: unset;">
+				<div class="perso3">
+					<p>Dylan</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- web/pages/presentation.php
+++ web/pages/presentation.php
@@ -3,24 +3,29 @@
         <title>Présentation de notre site</title>
     </head>
     <body>
-        <div class="body">
+        <div class="body" style="background-color: #aabbff;" >
             
-            <div class="prese_horaire">
+            <div class="prese_horaire" style="display: flex; justify-content: space-around;">
                 <div class="presentation">
-                    <h3>Présentation</h3>
-                    <p class="presentation_site">
+                    <h3 class="titre_presentation" style="font-size: 51; text-shadow: 1px 1px 2px red, 0 0 1em blue, 0 0 0.2em blue; text-align: center;">
+                        Présentation
+                    </h3>
+                    <p class="presentation_site" style="font-size: 20; color: white;">
                         Bienvenue sur ce site, vous pourrez acheter tous types de bières, mais également des futs de bières, des apéros et autres.  
                     </p>
                 </div>
                 
-                <div class="horaire">
-                    <p class="horaire_site">
-                        
+                <div class="horaire" style="border-style: double; box-shadow: 10px 5px 5px grey;">
+                    <h3 class="titre_horaire" style="font-size: 51; text-shadow: 1px 1px 2px red, 0 0 1em blue, 0 0 0.2em blue;">
+                        Les horaires :
+                    </h3>
+                    <p class="horaire_site" style="background-color: beige;">
+                        Ouvert tout les jours de 5h00 jusqu'à 0h00
                     </p>
                 </div>
             </div> 
-            <div class="localisation">
-                <img src="" width="" height="" alt="alt"/>
+            <div class="localisation" style="background-color: skyblue; margin-top: 150px; border-style: dashed;">
+                <img src="https://www.google.fr/maps/@45.7864114,3.1383506,14.75z" width="100%" height="300" alt="localisation"/>
             </div>
         </div>     
     </body>
