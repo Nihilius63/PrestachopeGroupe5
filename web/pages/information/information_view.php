375a26307df4c669ff4943c14a131ef31f6bdeb5
Nathim <nathimrichar@gmail.com>
2021-04-30 16:58:34 +0200

    Information

--- web/pages/information/information_view.php
+++ web/pages/information/information_view.php
@@ -4,37 +4,42 @@
         <title>Prestachope</title>
         <link rel="stylesheet" href="assets/css/information.css"/>
         <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Uncial+Antiqua&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
     </head>
     <body>
-        <div class="head">
-            <a href="index.php?page=presentation">accueil</a>
-            <a href="index.php?page=info" class="active"> Information</a>
-        </div>
-        <div class="info">
-            <div class="text">
-                <div class="adresse">
-                    <h2>Adresse</h2>
-                    <p> 48 rue de Paris <br>
-                        03200 Vichy </p>
+        <div class="conteneur">
+            <div class="head">
+                <a href="index.php?page=presentation">accueil</a>
+                <a href="index.php?page=info" class="active"> Information</a>
+            </div>
+            <div class="info">
+                <div class="text">
+                    <div class="adresse">
+                        <h2><span>A</span>dresse</h2>
+                        <p> 48 rue de Paris <br>
+                            03200 Vichy </p>
+                    </div>
+                    <div class="telephone">
+                        <h2><span>T</span>éléphone</h2>
+                        <p> 07 76 87 67 76 </p>
+                    </div>
+                    <div class="horraire">
+                        <h2><span>H</span>oraires</h2>
+                        <p>Lundi: Fermé <br>
+                            Mardi: 8H-12h30   13H-18H <br>
+                            Mercredi: 8H-12h30   13H-18H <br>
+                            Jeudi: 8H-12h   12h30-19H <br>
+                            Vendredi: 8H-12h   12h30-19H</p>
+                    </div>
                 </div>
-                <div class="telephone">
-                    <h2>Téléphone</h2>
-                    <p> 07 76 87 67 76 </p>
-                </div>
-                <div class="horraire">
-                    <h2>Horaires</h2>
-                    <p>Lundi: Fermé <br>
-                        Mardi: 8H-12h30   13H-18H <br>
-                        Mercredi: 8H-12h30   13H-18H <br>
-                        Jeudi: 8H-12h   12h30-19H <br>
-                        Vendredi: 8H-12h   12h30-19H</p>
+                <div class="carte">
+                    <iframe style="filter: invert(90%)" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2765.2219845735317!2d3.4248326154540103!3d46.126407879114375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f6cd059a5f3193%3A0x533f5c568810664d!2s48%20Rue%20de%20Paris%2C%2003200%20Vichy!5e0!3m2!1sfr!2sfr!4v1619094556374!5m2!1sfr!2sfr" width="400" height="300" allowfullscreen="" loading="lazy"></iframe>
                 </div>
             </div>
-            <div class="carte">
-                <iframe style="filter: invert(90%)" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2765.2219845735317!2d3.4248326154540103!3d46.126407879114375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f6cd059a5f3193%3A0x533f5c568810664d!2s48%20Rue%20de%20Paris%2C%2003200%20Vichy!5e0!3m2!1sfr!2sfr!4v1619094556374!5m2!1sfr!2sfr" width="400" height="300" allowfullscreen="" loading="lazy"></iframe>
+            <div class="footer">
+                <p> <span>Designed & developed by </span> Nathim Dylan Lucas </p>
             </div>
         </div>
     </body>
