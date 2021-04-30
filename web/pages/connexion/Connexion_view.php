5f6dec7adfb1218f10e2405f8739fdb4b4b1ef1f
CleqrZ <dylangrangeon34@gmail.com>
2021-04-30 16:59:38 +0200

    css connexion+ inscription (commencement)

--- web/pages/connexion/Connexion_view.php
+++ web/pages/connexion/Connexion_view.php
@@ -2,14 +2,27 @@
 <html>
     <head>
         <title>Prestachope</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="assets/css/connexion.css"/>
     </head>
     <body>
-        <form action="" method="POST">
-            <input type="email" name="email" placeholder="Entrez votre email">
-            <input type="password" name="mdp" placeholder="Entrez votre mot de passe : ">
-            <input type="submit" value="Se connecter">
-        </form>
-        <a href="index.php?page=inscription"  value="inscription" >Pas encore de comtpe ? Créer en un</a>
+        <div class="body-form">
+            <div class="form">
+                <div class="nom-form">
+                    <h3>Connexion</h3>
+                </div>
+                <div class="main-form">
+                    <form action="" method="POST">
+                        <input type="email" name="email" placeholder="Entrez votre email">
+                        <input type="password" name="mdp" placeholder="Entrez votre mot de passe : ">
+                        <input type="submit" value="Se connecter">
+                    </form>
+                </div>
+            </div>
+            
+            <a href="index.php?page=inscription"  value="inscription" >Pas encore de comtpe ? Créer en un</a> 
+        </div>
+        
     </body>
 </html>
 
