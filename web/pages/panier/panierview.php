237efd217f72932e706ea5f91e2c75317f3a03fe
Nathim <nathimrichar@gmail.com>
2021-05-15 21:55:46 +0200

    page panier

--- web/pages/panier/panierview.php
+++ web/pages/panier/panierview.php
@@ -8,26 +8,34 @@ $content=$instanceController->testclear();
         <title>title</title>
     </head>
     <body>
-        <?php
-        include_once 'DAO/produitDAO.php';
-        include_once 'DTO/produitDTO.php';
-            $content=$instanceController->content();
-            $supertotal=0;
-            foreach ($content as $contents=>$values)
-            {
-                $total=0;
-                echo $contents.'<br>';
-                $produit=produitDAO::selectproduitbynom($contents);
-                echo $produit->getPrix().'<br>';
-                echo "Quantité: ".$values.'<br>';
-                $total=floatval($produit->getPrix())*$values;
-                echo "Total:".$total.'<br>';
-                $supertotal=$total+$supertotal;
-            }
-        
-       echo '<a href="index.php?page=commande">Valider ma commande '.$supertotal.'</a><br>';
-       echo '<a href="index.php?page=panier&panier=clear">Vider le panier</a>';
-       ?>
+        <div class="conteneur">
+            <h1> Panier </h1>
+            <?php
+            include_once 'DAO/produitDAO.php';
+            include_once 'DTO/produitDTO.php';
+                $content=$instanceController->content();
+                $supertotal=0;
+                foreach ($content as $contents=>$values)
+                {
+                    ?> 
+                    <div class="produit"> 
+                        <?php $total=0;
+                        echo $contents.'<br>';
+                        $produit=produitDAO::selectproduitbynom($contents);
+                        echo $produit->getPrix().'<br>';
+                        echo "Quantité: ".$values.'<br>';
+                        $total=floatval($produit->getPrix())*$values;
+                        echo "Total:".$total.'<br>';
+                        $supertotal=$total+$supertotal;
+                        ?> 
+                    </div>
+                    <?php
+                }
+
+           echo '<a href="index.php?page=commande">Valider ma commande '.$supertotal.'</a><br>';
+           echo '<a href="index.php?page=panier&panier=clear">Vider le panier</a>';
+           ?>
+        </div>
     </body>
 </html>
 
